6f696f2a592bbeae48142d280da11f1e3fa6f3fa
MSN Raju <mavuri.raju@gmail.com>
2020-07-05 14:07:46 +0530

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# fix-al-file-names README
+
+Use this extension to fix AL file names as per the best practices recommended by MicroSoft.
+
+## Features
+
+Run the Fix AL File Names command from the Command Palette (`Ctrl+Shift+P`) in the new window
+
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,39 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import FixALFileName from "./fix-al-file-name";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "fix-al-file-names" is now active!'
+  );
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "fix-al-file-names.fixAlFileNames",
+    async () => {
+      // The code you place here will be executed every time your command is executed
+
+      // Display a message box to the user
+      if (vscode.workspace.workspaceFolders)
+        await FixALFileName.fixFileNames(
+          vscode.workspace.workspaceFolders[0].uri.fsPath
+        );
+
+      vscode.window.showInformationMessage(
+        "AL file names corrected as per the best practices"
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
